5300fe43350ae5c44ae731dd6a07740567485e27
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-26 16:40:25 +0200

    HireMeLink component created, with its styled-components styles

---
+++ src/HireMeLink/index.js
@@ -0,0 +1,8 @@
+import { LinkWrapper, LinkText, StyledMessageIcon } from "./styled";
+
+export const HireMeLink = () => (
+    <LinkWrapper href="mailto:oskarwlaszczuk@gmail.com">
+        <StyledMessageIcon />
+        <LinkText>Hire Me</LinkText>
+    </LinkWrapper>
+);
