de71bad34c66aa4c503f84bcea7aa5cc674a35e9
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-26 10:42:25 +0200

    Alternative box-sizing added

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,14 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-body {
-  margin: 0;
-  font-family: "Inter", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  background-color: 
-    ${({ theme }) =>
-    theme.lightTheme.backgroundColors.WhiteLilac
+  html {
+    box-sizing: border-box;
   };
-};
+
+  *, ::after, ::before {
+    box-sizing: inherit;
+  };
+
+  body {
+    margin: 0;
+    font-family: "Inter", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: 
+      ${({ theme }) =>
+      theme.lightTheme.backgroundColors.WhiteLilac
+    };
+  };
 `;
