b54de2e095db236ca5f8253d24941dfb76eab539
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-26 17:21:01 +0200

    Wrapped component's content (except Image styled-component) into Content styled-component

--- src/AboutMe/index.js
+++ src/AboutMe/index.js
@@ -1,13 +1,15 @@
-import { Wrapper, Image, Caption, Header, Description, ExtraContent } from "./styled";
+import { Wrapper, Image, Caption, Header, Description, ExtraContent, Content } from "./styled";
 
 export const AboutMe = (
     { image, caption, header, description, extraContent }
 ) => (
     <Wrapper>
         <Image src={image} alt="" />
-        <Caption>{caption}</Caption>
-        <Header>{header}</Header>
-        <Description>{description}</Description>
-        <ExtraContent>{extraContent}</ExtraContent>
+        <Content>
+            <Caption>{caption}</Caption>
+            <Header>{header}</Header>
+            <Description>{description}</Description>
+            <ExtraContent>{extraContent}</ExtraContent>
+        </Content>
     </Wrapper>
 );
