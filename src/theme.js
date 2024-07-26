517488abe379b6713d2eb70326a7c61bc4de020a
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-26 20:16:39 +0200

    Added 'iron' color

--- src/theme.js
+++ src/theme.js
@@ -6,8 +6,9 @@ export const theme = {
             white: "#FFFFFF",
             whiteLilac: "#FBFBFE",
             scienceBlue: "#0366D6",
-            lightScienceBlue: "#D1D5DA4D",
+            lightScienceBlue: "#D1D5DA4D", 
             anakiwa: "#8CC2FF",
+            iron: "#D1D5DA",
         },
     },
 };
