65a6991cecf2305a40828cf35b038c507fd9ae77
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-26 16:38:34 +0200

    Moved all colors into 'colors' property, removed 'backgroundColors' property

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,13 @@
 export const theme = {
     lightTheme: {
-        backgroundColors: {
-            WhiteLilac: "#FBFBFE",
+        colors: {
+            slateGray: "#6E7E91",
+            mineShaft: "#252525",
+            white: "#FFFFFF",
+            whiteLilac: "#FBFBFE",
+            scienceBlue: "#0366D6",
+            lightScienceBlue: "#D1D5DA4D",
+            anakiwa: "#8CC2FF",
         },
     },
 };
