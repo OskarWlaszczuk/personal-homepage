e517fb86cd848fdbafb3ab4244382b12d871c261
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-26 16:35:02 +0200

    Imported AboutMe component into App.js and added props to AboutMe

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { AboutMe } from "./AboutMe";
+import { HireMeLink } from "./HireMeLink";
+
 function App() {
   return (
-    <></>
+    <>
+      <AboutMe
+        image="
+          https://avatars.githubusercontent.com/u/155220171?v=4
+        "
+        caption="This is"
+        header="Oskar Właszczuk"
+        description="
+          I'm a passionate Frontend Developer in love with React,currently looking for new job opportunities.
+        "
+        extraContent={<HireMeLink />}
+      />
+    </>
   );
 }
 
