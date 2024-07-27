486f1fc2cda15e23d8c0e2f805f1dc3000f5c39b
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-27 09:38:30 +0200

    Added new colors

--- src/theme.js
+++ src/theme.js
@@ -6,9 +6,15 @@ export const theme = {
             white: "#FFFFFF",
             whiteLilac: "#FBFBFE",
             scienceBlue: "#0366D6",
-            lightScienceBlue: "#D1D5DA4D", 
+            lightScienceBlue: "#D1D5DA4D",
             anakiwa: "#8CC2FF",
             iron: "#D1D5DA",
+
+        },
+    },
+    darkTheme: {
+        colors: {
+            dodgerBlue: "#2188FF",
         },
     },
 };
