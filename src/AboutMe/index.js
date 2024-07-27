d2ebb5accd4133065329de1b6149ed722a6bd578
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-27 09:41:30 +0200

    Add isImageExists variable to handle conditional rendering of image The isImageExists variable is a boolean that determines whether an image exists. This variable is used to conditionally render the Image component and to adjust styles in the Section and Header components.

--- src/AboutMe/index.js
+++ src/AboutMe/index.js
@@ -1,17 +1,19 @@
 import { Section, Image, Caption, Header, Description, ExtraContent, Content } from "./styled";
 
-export const AboutMe = (
-    { image, caption, header, description, extraContent }
-) => (
-    <Section $withoutImage={!image ? true : false}>
-        {image && (
-            <Image src={image} alt="" />
-        )}
-        <Content>
-            <Caption>{caption}</Caption>
-            <Header>{header}</Header>
-            <Description>{description}</Description>
-            <ExtraContent>{extraContent}</ExtraContent>
-        </Content>
-    </Section>
-);
+export const AboutMe = ({ image, caption, header, description, extraContent }) => {
+    const isImageExists = !image;
+
+    return (
+        <Section $withoutImage={isImageExists}>
+            {!isImageExists && (
+                <Image src={image} alt="" />
+            )}
+            <Content>
+                <Caption>{caption}</Caption>
+                <Header $withoutImage={isImageExists}>{header}</Header>
+                <Description>{description}</Description>
+                <ExtraContent>{extraContent}</ExtraContent>
+            </Content>
+        </Section>
+    )
+};
