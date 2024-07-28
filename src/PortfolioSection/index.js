b7d6ab88421d46c3c2bf37d64e74b56381d1a914
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-28 20:24:59 +0200

    Portfolio section created, it gets response of GH repositories and iterate on it, using ma(), for every received repo, creates seperate section, with repo's details

---
+++ src/PortfolioSection/index.js
@@ -0,0 +1,54 @@
+import { Container, HeaderContent, Header, SubHeader, StyledGitHubIcon, Repositories, Repo, RepoName, RepoDescription, RepoRepository, RepoHomepage, RepoLinksList, RepoLinkTitle, RepoLink } from "./styled";
+import { useGitHubRepoApi } from "../useGitHubRepoApi";
+import { Error } from "./Error";
+
+export const PortfolioSection = () => {
+    const { repositories, status } = useGitHubRepoApi()
+
+    return (
+        <Container>
+            <HeaderContent>
+                <StyledGitHubIcon></StyledGitHubIcon>
+                <Header>Portfolio</Header>
+                <SubHeader>My recent projects</SubHeader>
+            </HeaderContent>
+            {
+                status === "loading" ?
+                    <>Chiwleczkę</> :
+                    (
+                        status === "error" ?
+                            <Error />
+                            :
+                            <Repositories>
+                                {
+                                    repositories.map((
+                                        { name, description, html_url, homepage }
+                                    ) => (
+                                        <Repo key={name}>
+                                            <RepoName>
+                                                {name.charAt(0).toUpperCase() + name.slice(1)}
+                                            </RepoName>
+                                            <RepoDescription>{description}</RepoDescription>
+
+                                            <RepoLinksList>
+
+                                                <RepoHomepage>
+                                                    <RepoLinkTitle>Demo:</RepoLinkTitle>
+                                                    <RepoLink key={homepage} href={homepage}> {homepage}</RepoLink>
+                                                </RepoHomepage>
+
+                                                <RepoRepository>
+                                                    <RepoLinkTitle>Code:</RepoLinkTitle>
+                                                    <RepoLink key={html_url} href={html_url}> {html_url}</RepoLink>
+                                                </RepoRepository>
+                                            </RepoLinksList>
+
+                                        </Repo>
+                                    ))
+                                }
+                            </Repositories>
+                    )
+            }
+        </Container>
+    )
+};
