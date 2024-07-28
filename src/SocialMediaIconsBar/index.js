7e567ad58d2d17fad56942ed39a71599c0715339
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-28 20:27:53 +0200

    SocialMediaIconsBar component, with its styled-components created

---
+++ src/SocialMediaIconsBar/index.js
@@ -0,0 +1,10 @@
+import { StyledGitHubIcon, StyledFacebookIcon, StyledLinkedInIcon, StyledInstagramIcon, Wrapper } from "./styled";
+
+export const SocialMediaIconsBar = () => (
+    <Wrapper>
+        <StyledGitHubIcon />
+        <StyledFacebookIcon />
+        <StyledLinkedInIcon />
+        <StyledInstagramIcon />
+    </Wrapper>
+);
---
+++ src/SocialMediaIconsBar/styled.js
@@ -0,0 +1,68 @@
+import styled from "styled-components"
+import { ReactComponent as FacebookIcon } from "../svgs/Facebook_black.svg";
+import { ReactComponent as LinkedInIcon } from "../svgs/LinkedIN_black.svg";
+import { ReactComponent as InstagramIcon } from "../svgs/Instagram_black.svg";
+import { ReactComponent as GitHubIcon } from "../svgs/Github.svg";
+
+export const Wrapper = styled.section`
+    display: grid;
+    grid-template-columns: repeat(4, 48px);
+    grid-gap: 24px;
+`;
+
+export const StyledGitHubIcon = styled(GitHubIcon)`
+    path {
+        fill: ${({ theme }) => theme.lightTheme.colors.mineShaft};
+        transform: 0.5s;
+    };
+
+    &:hover {
+        path {
+            fill: ${({ theme }) => theme.lightTheme.colors.scienceBlue};
+ 
+        };
+    };
+`;
+
+export const StyledFacebookIcon = styled(FacebookIcon)`
+    path {
+        fill: ${({ theme }) => theme.lightTheme.colors.mineShaft};
+        transform: 0.5s;
+    };
+
+    &:hover {
+        path {
+            fill: ${({ theme }) => theme.lightTheme.colors.scienceBlue};
+ 
+        };
+    };
+`;
+
+export const StyledLinkedInIcon = styled(LinkedInIcon)`
+     path {
+        fill: ${({ theme }) => theme.lightTheme.colors.mineShaft};
+        transform: 0.5s;
+    };
+
+    &:hover {
+        path {
+            fill: ${({ theme }) => theme.lightTheme.colors.scienceBlue};
+ 
+        };
+    };
+`;
+
+
+export const StyledInstagramIcon = styled(InstagramIcon)`
+    path {
+        fill: ${({ theme }) => theme.lightTheme.colors.mineShaft};
+        transform: 1s;
+    };
+
+    &:hover {
+        path {
+            fill: ${({ theme }) => theme.lightTheme.colors.scienceBlue};
+ 
+        };
+    };
+`;
