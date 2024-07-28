4b4714562a7356814b67c676961074d0a03b4beb
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-28 20:28:16 +0200

    MessageIcon moved to seperated folder

---
+++ src/StyledMessageIcon/index.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { ReactComponent as MessageIcon } from "../svgs/Message.svg"
+
+export const StyledMessageIcon = styled(MessageIcon)`
+    max-height: 24px;
+`;
