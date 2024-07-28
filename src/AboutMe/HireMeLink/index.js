6c7fc2677a67809e087ab3f0864e0c7907373211
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-28 20:21:40 +0200

    File's folder renamed to more generic LinkBox

--- src/AboutMe/HireMeLink/index.js
+++
@@ -1,8 +0,0 @@
-import { LinkWrapper, LinkText, StyledMessageIcon } from "./styled";
-
-export const HireMeLink = () => (
-    <LinkWrapper href="mailto:oskarwlaszczuk@gmail.com">
-        <StyledMessageIcon />
-        <LinkText>Hire Me</LinkText>
-    </LinkWrapper>
-);
