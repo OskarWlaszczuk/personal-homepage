655e20f609e1dc43b1904310a123a86b42089477
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-29 19:18:14 +0200

    Moved Message, and Title styled-components to seperated folders

--- src/PortfolioSection/Error/styled.js
+++ src/PortfolioSection/Error/styled.js
@@ -1,24 +1,11 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    margin: 88px 0;
-    text-align: center;
-    display: grid;
-    grid-template-columns: 1fr;
-    justify-items: center;
-    grid-gap: 32px;   
-`;
 export const Title = styled.h3`
     font-size: 24px;
     font-weight: 700;
     margin: 0;
-`;
 
-export const Description = styled.p`
-    color: ${({ theme }) =>
-        theme.lightTheme.colors.mineShaft
+    @media (max-width: ${({ theme }) => theme.breakPoints.mobile}px) {
+        font-size: 18px;
     };
-    font-size: 20px;
-    font-weight: 400;
-    margin: 0;
 `;
