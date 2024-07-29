dbcdbb1f043f21de1ae4d669ba975ef2bf228707
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-29 19:10:05 +0200

    New property 'breakPoints' added, with diffrent devices dimensions

--- src/theme.js
+++ src/theme.js
@@ -4,7 +4,7 @@ export const theme = {
             slateGray: "#6E7E91",
             mineShaft: "#252525",
             white: "#FFFFFF",
-            whiteLilac: "#FBFBFE",
+            whiteLilac: "#eee",
             scienceBlue: "#0366D6",
             lightScienceBlue: "#D1D5DA4D",
             anakiwa: "#8CC2FF",
@@ -17,5 +17,12 @@ export const theme = {
         colors: {
             dodgerBlue: "#2188FF",
         },
+    },
+    breakPoints : {
+        laptop: 1200,
+        smallLaptop: 950,
+        tablet: 730,
+        mobileXl: 550,
+        mobile: 350,
     },
 };
