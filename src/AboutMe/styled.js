e6ba3316b61ed2aaff151d84893a8a1c3cb994fa
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-29 19:13:43 +0200

    Styles for narrower devices added

--- src/AboutMe/styled.js
+++ src/AboutMe/styled.js
@@ -4,10 +4,19 @@ export const Section = styled.section`
     display: grid;
     grid-gap: 72px;
     grid-template-columns: auto 1fr;
-    max-width: 1089px;
-    max-height: 384px;
-    margin: auto;
+    justify-self: baseline;
 
+    @media (max-width: ${({ theme }) => theme.breakPoints.smallLaptop}px) {
+        grid-template-columns: 1fr;
+        grid-gap: 16px;
+        width: 100%
+    };
+
+    @media (max-width: ${({ theme }) => theme.breakPoints.tablet}px) {
+        grid-template-columns: 100%;
+        grid-gap: 16px;
+    };
+    
     ${({ $withoutImage }) => $withoutImage && css`
         margin: 0px;
         grid-gap: 0px;
@@ -16,13 +25,30 @@ export const Section = styled.section`
 `;
 
 export const Content = styled.article`
+    display: grid;
+    grid-template-columns: 1fr;
     align-self: center;
+    grid-gap: 24px;
+
+    @media (max-width: ${({ theme }) => theme.breakPoints.mobile}px) {
+        grid-gap: 12px;
+    };
 `;
 
 export const Image = styled.img`
     max-width: 398px;
     max-height: 398px;
     border-radius: 50%;
+
+    @media (max-width: ${({ theme }) => theme.breakPoints.smallLaptop}px) {
+        margin: auto;
+    };
+
+    @media (max-width: ${({ theme }) => theme.breakPoints.mobileXl}px) {
+        max-width: 135px;
+        max-height: 135px;
+        margin: 0;
+    };
 `;
 
 export const Caption = styled.header`
@@ -36,12 +62,15 @@ export const Caption = styled.header`
 export const Header = styled.h1`
     font-size: 38px;
     font-weight: 900;
-    margin: 12px 36px 0 0;
+    margin: 0;
     color: ${({ theme }) =>
         theme.lightTheme.colors.mineShaft
     };
     transition: 0.5s;
 
+    @media (max-width: ${({ theme }) => theme.breakPoints.mobile}px) {
+        font-size: 19px;
+    };
 
     ${({ $withoutImage }) => $withoutImage && css`
         &:hover {
@@ -53,12 +82,16 @@ export const Header = styled.h1`
 `;
 
 export const Description = styled.p`
-    max-width: 670px;
+    width: 50%;
+    margin: 0;
     font-size: 20px;
     font-size: 18px;
     font-weight: 400;
-    margin-bottom: 32px;
     color: ${({ theme }) =>
         theme.lightTheme.colors.slateGray
+    };
+
+    @media (max-width: ${({ theme }) => theme.breakPoints.mobile}px) {
+        width: unset;
     };
 `;
