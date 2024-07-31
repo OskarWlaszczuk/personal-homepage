9e570960934c6e124f524631388e7bc2d5fa1d4d
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-31 17:47:43 +0200

    Moved components inside Provider, with imported store as prop

--- src/index.js
+++ src/index.js
@@ -6,14 +6,18 @@ import reportWebVitals from './reportWebVitals';
 import { theme } from './theme';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './GlobalStyles.js';
+import { Provider } from 'react-redux';
+import { store } from './store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
