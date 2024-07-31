e84fc225e5cdde67ae5e3cd9c77b535bbf4a90b8
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-31 20:17:35 +0200

    fixed Caption import

--- src/AboutMe/index.js
+++ src/AboutMe/index.js
@@ -1,24 +1,30 @@
 import { Section, Image, Header, Description, Content } from "./styled";
-import { Caption } from "../Caption"
+import { Caption } from "../common/Caption";
 import { useSelector } from "react-redux";
 import { selectIsDarkTheme } from "../themeSlice";
 
-export const AboutMe = ({ image, caption, header, description, extraContent }) => {
-    const isImageExists = !image;
+export const AboutMe = ({
+  image,
+  caption,
+  header,
+  description,
+  extraContent,
+}) => {
+  const isImageExists = !image;
 
-    const isDarkTheme = useSelector(selectIsDarkTheme);
+  const isDarkTheme = useSelector(selectIsDarkTheme);
 
-    return (
-        <Section $withoutImage={isImageExists}>
-            {!isImageExists && (
-                <Image src={image} alt="" />
-            )}
-            <Content>
-                <Caption content={caption}/>
-                <Header $isDarkTheme={isDarkTheme} $withoutImage={isImageExists}>{header}</Header>
-                <Description $isDarkTheme={isDarkTheme}>{description}</Description>
-                <>{extraContent}</>
-            </Content>
-        </Section>
-    )
+  return (
+    <Section $withoutImage={isImageExists}>
+      {!isImageExists && <Image src={image} alt="" />}
+      <Content>
+        <Caption content={caption} />
+        <Header $isDarkTheme={isDarkTheme} $withoutImage={isImageExists}>
+          {header}
+        </Header>
+        <Description $isDarkTheme={isDarkTheme}>{description}</Description>
+        <>{extraContent}</>
+      </Content>
+    </Section>
+  );
 };
