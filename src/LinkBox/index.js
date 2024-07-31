65d9dc7cdf7d89da96adc8f7c6ba9fc85ae1dff7
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-31 18:01:15 +0200

    Setting theme, depending on isDarkTheme state

--- src/LinkBox/index.js
+++ src/LinkBox/index.js
@@ -1,13 +1,15 @@
+import { useSelector } from "react-redux";
 import { LinkWrapper, LinkText } from "./styled";
+import { selectIsDarkTheme } from "../themeSlice";
 
 export const LinkBox = ({ href, text, Icon }) => {
     const isIconExists = !Icon
-
+    const isDarkTheme = useSelector(selectIsDarkTheme);
     return (
         <LinkWrapper
             $withoutIcon={isIconExists}
-            target="_blank"
             href={href}
+            $isDarkTheme={isDarkTheme}
         >
             {!isIconExists && (
                 Icon
