f5f9be9be1faa70ab73b8a57c03924edb1ccf065
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-31 18:06:10 +0200

    setting theme styles, depending on isDarkTheme state

--- src/PortfolioSection/Error/styled.js
+++ src/PortfolioSection/Error/styled.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const Title = styled.h3`
+   color: ${({ theme, $isDarkTheme }) =>
+        !$isDarkTheme &&
+        theme.lightTheme.colors.mineShaft
+    };
     font-size: 24px;
     font-weight: 700;
     margin: 0;
