f821caae78d35b43979a9fdcc100f377a29ea67e
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-31 18:03:41 +0200

    StyledMessage styled-component declared, added Message component styles inside

---
+++ src/Message/styled..js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledMessage = styled.p`
+   color: ${({ theme, $isDarkTheme }) =>
+        !$isDarkTheme &&
+        theme.lightTheme.colors.mineShaft
+    };
+    font-size: 20px;
+    font-weight: 400;
+    margin: 0;
+
+    @media (max-width: ${({ theme }) => theme.breakPoints.mobile}px) {
+        font-size: 17px;
+    };
+`;
