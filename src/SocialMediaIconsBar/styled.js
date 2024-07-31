4d98fd66ebe76b7b492ec4d752de2d5d1add7da3
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-31 20:36:54 +0200

    code formatted

--- src/SocialMediaIconsBar/styled.js
+++ src/SocialMediaIconsBar/styled.js
@@ -1,105 +1,91 @@
-import styled from "styled-components"
+import styled from "styled-components";
 import { ReactComponent as FacebookIcon } from "../svgs/Facebook_black.svg";
 import { ReactComponent as LinkedInIcon } from "../svgs/LinkedIN_black.svg";
 import { ReactComponent as InstagramIcon } from "../svgs/Instagram_black.svg";
 import { ReactComponent as GitHubIcon } from "../svgs/Github.svg";
 
 export const Wrapper = styled.section`
-    display: grid;
-    grid-template-columns: repeat(4, 48px);
-    grid-gap: 24px;
-    margin-top: 56px;
+  display: grid;
+  grid-template-columns: repeat(4, 48px);
+  grid-gap: 24px;
+  margin-top: 56px;
 
-    @media (max-width: ${({ theme }) => theme.breakPoints.mobile}px) {
-        margin-top: 40px;
-    };
+  @media (max-width: ${({ theme }) => theme.breakPoints.mobile}px) {
+    margin-top: 40px;
+  }
 `;
 
-export const Link = styled.a`
-    
-`;
+export const Link = styled.a``;
 
 export const StyledGitHubIcon = styled(GitHubIcon)`
-    path {
-        fill: ${({ theme, $isDarkTheme }) =>
-        $isDarkTheme ?
-            theme.darkTheme.colors.white :
-            theme.lightTheme.colors.mineShaft
-    };
-        transform: 0.5s;
-    };
+  path {
+    fill: ${({ theme, $isDarkTheme }) =>
+      $isDarkTheme
+        ? theme.darkTheme.colors.white
+        : theme.lightTheme.colors.mineShaft};
+  }
 
-    &:hover {
-        path {
-            fill: ${({ theme, $isDarkTheme }) =>
-        $isDarkTheme ?
-            theme.darkTheme.colors.dodgerBlue :
-            theme.lightTheme.colors.scienceBlue
-    };
-        };
-    };
+  &:hover {
+    path {
+      fill: ${({ theme, $isDarkTheme }) =>
+        $isDarkTheme
+          ? theme.darkTheme.colors.dodgerBlue
+          : theme.lightTheme.colors.scienceBlue};
+    }
+  }
 `;
 
 export const StyledFacebookIcon = styled(FacebookIcon)`
-    path {
-        fill: ${({ theme, $isDarkTheme }) =>
-        $isDarkTheme ?
-            theme.darkTheme.colors.white :
-            theme.lightTheme.colors.mineShaft
-    };
-        transform: 0.5s;
-    };
+  path {
+    fill: ${({ theme, $isDarkTheme }) =>
+      $isDarkTheme
+        ? theme.darkTheme.colors.white
+        : theme.lightTheme.colors.mineShaft};
+  }
 
-    &:hover {
-        path {
-            fill: ${({ theme, $isDarkTheme }) =>
-        $isDarkTheme ?
-            theme.darkTheme.colors.dodgerBlue :
-            theme.lightTheme.colors.scienceBlue
-    };
-        };
-    };
+  &:hover {
+    path {
+      fill: ${({ theme, $isDarkTheme }) =>
+        $isDarkTheme
+          ? theme.darkTheme.colors.dodgerBlue
+          : theme.lightTheme.colors.scienceBlue};
+    }
+  }
 `;
 
 export const StyledLinkedInIcon = styled(LinkedInIcon)`
-     path {
-        fill: ${({ theme, $isDarkTheme }) =>
-        $isDarkTheme ?
-            theme.darkTheme.colors.white :
-            theme.lightTheme.colors.mineShaft
-    };
-        transform: 0.5s;
-    };
+  path {
+    fill: ${({ theme, $isDarkTheme }) =>
+      $isDarkTheme
+        ? theme.darkTheme.colors.white
+        : theme.lightTheme.colors.mineShaft};
+  }
 
-    &:hover {
-        path {
-            fill: ${({ theme, $isDarkTheme }) =>
-        $isDarkTheme ?
-            theme.darkTheme.colors.dodgerBlue :
-            theme.lightTheme.colors.scienceBlue
-    };
-        };
-    };
+  &:hover {
+    path {
+      fill: ${({ theme, $isDarkTheme }) =>
+        $isDarkTheme
+          ? theme.darkTheme.colors.dodgerBlue
+          : theme.lightTheme.colors.scienceBlue};
+    }
+  }
 `;
 
-
 export const StyledInstagramIcon = styled(InstagramIcon)`
-    path {
-        fill: ${({ theme, $isDarkTheme }) =>
-        $isDarkTheme ?
-            theme.darkTheme.colors.white :
-            theme.lightTheme.colors.mineShaft
-    };
-        transform: 1s;
-    };
+  path {
+    fill: ${({ theme, $isDarkTheme }) =>
+      $isDarkTheme
+        ? theme.darkTheme.colors.white
+        : theme.lightTheme.colors.mineShaft};
+    transform: 1s;
+  }
 
-    &:hover {
-        path {
-            fill: ${({ theme, $isDarkTheme }) =>
-        $isDarkTheme ?
-            theme.darkTheme.colors.dodgerBlue :
-            theme.lightTheme.colors.scienceBlue
-    };
-        };
-    };
+  &:hover {
+    path {
+      fill: ${({ theme, $isDarkTheme }) =>
+        $isDarkTheme
+          ? theme.darkTheme.colors.dodgerBlue
+          : theme.lightTheme.colors.scienceBlue};
+    }
+  }
 `;
