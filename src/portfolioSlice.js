59c59d671e10b48bbd63d58f3faf4832940ffa41
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-31 20:00:59 +0200

    fixed typo in fetchRepositoriesSuccess action

--- src/portfolioSlice.js
+++ src/portfolioSlice.js
@@ -1,28 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const portfolioSlice = createSlice({
-    name: "portfolio",
-    initialState: {
-        repositories: [],
-        status: "loading"
+  name: "portfolio",
+  initialState: {
+    repositories: [],
+    status: "loading",
+  },
+  reducers: {
+    fetchRepositories: () => {},
+    fetchRepositoriesSuccess: (state, { payload: repositories }) => {
+      state.repositories = repositories;
+      state.status = "succes;";
     },
-    reducers: {
-        fetchRepositories: () => { },
-        fetchRepositoriesSuccess: (state, { payload: repositories }) => {
-            state.repositories = repositories;
-            state.status = "succes;"
-        },
-        fetchRepositoriesError: (state) => {
-            state.status = "error";
-        },
-    }
+    fetchRepositoriesError: (state) => {
+      state.status = "error";
+    },
+  },
 });
 
-export const { fetchRepositories, fetchRepositoriesSuccess, fetchRepositoriesError } = portfolioSlice.actions;
+export const {
+  fetchRepositories,
+  fetchRepositoriesSuccess,
+  fetchRepositoriesError,
+} = portfolioSlice.actions;
 
-export const selectPortfolioState = state => state.portfolio;
+export const selectPortfolioState = (state) => state.portfolio;
 
-export const selectRepositories = state => selectPortfolioState(state).repositories;
-export const selectRepositoriesStatus = state => selectPortfolioState(state).status;
+export const selectRepositories = (state) =>
+  selectPortfolioState(state).repositories;
+export const selectRepositoriesStatus = (state) =>
+  selectPortfolioState(state).status;
 
 export const portfolioReducer = portfolioSlice.reducer;
