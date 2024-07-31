0724f147df9ab0fa4e0cca782462b99c0b94767e
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-31 18:06:03 +0200

    setting theme styles, depending on isDarkTheme state

--- src/PortfolioSection/Error/index.js
+++ src/PortfolioSection/Error/index.js
@@ -2,16 +2,20 @@ import { Title } from "./styled"
 import { Message } from "../../Message"
 import { Container } from "../../Container"
 import { LinkBox } from "../../LinkBox"
+import { useSelector } from "react-redux"
+import { selectIsDarkTheme } from "../../themeSlice"
 import { ReactComponent as ErrorIcon } from "../../svgs/Error.svg"
 
 export const Error = () => {
+    const isDarkTheme = useSelector(selectIsDarkTheme);
+
     return (
         <Container>
             <ErrorIcon />
-            <Title>Ooops! Something went wrong...</Title>
-            <Message>
+            <Title $isDarkTheme={isDarkTheme}>Ooops! Something went wrong...</Title>
+            <Message content={<>
                 Sorry, failed to load Github projects. <br />You can check them directly on Github.
-            </Message>
+            </>} />
             <LinkBox
                 href="https://github.com/OskarWlaszczuk/personal-homepage.git"
                 text="Go to Github"
