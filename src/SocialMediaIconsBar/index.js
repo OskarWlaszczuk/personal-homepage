cd5b5ad1006549c0a6c600965fac99aa70898b50
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-31 18:07:34 +0200

    setting theme styles, depending on isDarkTheme state, Icons moved inside link elements

--- src/SocialMediaIconsBar/index.js
+++ src/SocialMediaIconsBar/index.js
@@ -1,10 +1,31 @@
-import { StyledGitHubIcon, StyledFacebookIcon, StyledLinkedInIcon, StyledInstagramIcon, Wrapper } from "./styled";
+import { useSelector } from "react-redux";
+import {
+    StyledGitHubIcon,
+    StyledFacebookIcon,
+    StyledLinkedInIcon,
+    StyledInstagramIcon,
+    Wrapper,
+    Link,
+} from "./styled";
+import { selectIsDarkTheme } from "../themeSlice";
 
-export const SocialMediaIconsBar = () => (
-    <Wrapper>
-        <StyledGitHubIcon />
-        <StyledFacebookIcon />
-        <StyledLinkedInIcon />
-        <StyledInstagramIcon />
-    </Wrapper>
-);
+export const SocialMediaIconsBar = () => {
+    const isDarkTheme = useSelector(selectIsDarkTheme);
+    
+    return (
+        <Wrapper>
+            <Link href="https://github.com/OskarWlaszczuk" target="_blank">
+                <StyledGitHubIcon $isDarkTheme={isDarkTheme} />
+            </Link>
+            <Link href="https://www.facebook.com/oskarwlaszczuk" target="_blank">
+                <StyledFacebookIcon $isDarkTheme={isDarkTheme} />
+            </Link>
+            <Link href="https://www.linkedin.com/in/oskar-w%C5%82aszczuk-85307a1ab/" target="_blank">
+                <StyledLinkedInIcon $isDarkTheme={isDarkTheme} />
+            </Link>
+            <Link href="https://www.instagram.com/owlaszczuk/" target="_blank">
+                <StyledInstagramIcon $isDarkTheme={isDarkTheme} />
+            </Link>
+        </Wrapper>
+    );
+};
