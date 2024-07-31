5eca9e58e1aaeb894d9fdf9fe00bc5da066ccd7c
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-31 18:05:50 +0200

    setting theme styles, depending on isDarkTheme state

--- src/PortfolioSection/styled.js
+++ src/PortfolioSection/styled.js
@@ -23,29 +23,40 @@ export const Header = styled.h2`
     margin: 12px auto;
     font-size: 30px;
     font-weight: 900;
+    color: ${({ theme, $isDarkTheme }) =>
+    !$isDarkTheme &&
+    theme.lightTheme.colors.mineShaft
+  };
 
     @media (max-width: ${({ theme }) => theme.breakPoints.mobile}px) {
       font-size: 18px;
    };
 `;
 
-
 export const SubHeader = styled.p`
     font-size: 28px;
     font-weight: 400;
     margin: 0;
+    color: ${({ theme, $isDarkTheme }) =>
+      !$isDarkTheme &&
+      theme.lightTheme.colors.mineShaft
+  };
 
     @media (max-width: ${({ theme }) => theme.breakPoints.mobile}px) {
       font-size: 17px;
    };
 `;
 
-
 export const StyledGitHubIcon = styled(GitHubIcon)`
     max-width: 40px;
     max-height: 40px;
+
+    @media (max-width: ${({ theme }) => theme.breakPoints.mobile}px) {
+      max-width: 32px;
+      max-height: 32px;
+    };
     path {
-        fill: ${({ theme }) =>
+    fill: ${({ theme }) =>
     theme.lightTheme.colors.scienceBlue};
     };
 `;
@@ -72,24 +83,27 @@ export const Repo = styled.li`
     display: grid;
     width: 100%;
     grid-template-columns: 100%;
-    box-shadow: 0px -2px 50px 0px #090A3305;
-    background-color: #FFFFFF;
-    border: 6px solid #D1D5DA4D;
+    background-color: ${({ theme, $isDarkTheme }) =>
+    $isDarkTheme ?
+      theme.darkTheme.colors.lightMineShaft :
+      theme.lightTheme.colors.white
+  };
+
+    border: 6px solid ${({ theme }) => theme.lightTheme.colors.lightScienceBlue};
     transition: 0.3s;
     padding: 56px;
 
     &:hover{
-      border: 6px solid ${({ theme }) => theme.lightTheme.colors.cornflowerBlue};
+      border: 6px solid  ${({ theme, $isDarkTheme }) =>
+    $isDarkTheme ?
+      theme.darkTheme.colors.grayScienceBlue :
+      theme.lightTheme.colors.cornflowerBlue};
     };
 
     @media (max-width: ${({ theme }) => theme.breakPoints.laptop}px) {
       width: 100%;
       min-height: 400px;
     };
-
-    @media (max-width: ${({ theme }) => theme.breakPoints.mobileXl}px) {
-      padding: 24px;
-   };
 
    @media (max-width: ${({ theme }) => theme.breakPoints.mobile}px) {
       padding: 24px;
@@ -98,8 +112,10 @@ export const Repo = styled.li`
 `;
 
 export const RepoName = styled.h3`
-   color: ${({ theme }) =>
-    theme.lightTheme.colors.scienceBlue
+ color: ${({ theme, $isDarkTheme }) =>
+    $isDarkTheme ?
+      theme.darkTheme.colors.white :
+      theme.lightTheme.colors.scienceBlue
   };
     font-size: 24px;
     font-weight: 700;
@@ -111,8 +127,10 @@ export const RepoName = styled.h3`
 `;
 
 export const RepoDescription = styled.p`
-  color: ${({ theme }) =>
-    theme.lightTheme.colors.slateGray
+  color: ${({ theme, $isDarkTheme }) =>
+    $isDarkTheme ?
+      theme.darkTheme.colors.white :
+      theme.lightTheme.colors.slateGray
   };
   font-size: 18px;
   font-weight: 400;
@@ -130,8 +148,10 @@ export const RepoLinksList = styled.ul`
 `;
 
 export const RepoLinkTitle = styled.span`
-  color: ${({ theme }) =>
-    theme.lightTheme.colors.slateGray
+ color: ${({ theme, $isDarkTheme }) =>
+    $isDarkTheme ?
+      theme.darkTheme.colors.white :
+      theme.lightTheme.colors.slateGray
   };
   font-size: 18px;
   font-weight: 400;
@@ -159,14 +179,20 @@ export const RepoHomepage = styled.li`
 `;
 
 export const RepoLink = styled.a`
-  color: ${({ theme }) =>
-    theme.lightTheme.colors.scienceBlue
+  color: ${({ theme, $isDarkTheme }) =>
+    $isDarkTheme ?
+      theme.darkTheme.colors.dodgerBlue :
+      theme.lightTheme.colors.scienceBlue
   };
   font-size: 18px;
   font-weight: 400;
   line-height: 25.2px;
   text-decoration: none;
-  border-bottom: 1px solid #0366D6;
+  border-bottom: 1px solid ${({ theme, $isDarkTheme }) =>
+    $isDarkTheme ?
+      theme.darkTheme.colors.dodgerBlue :
+      theme.lightTheme.colors.scienceBlue
+  };;
   transition: 0.3s;
 
   @media (max-width: ${({ theme }) => theme.breakPoints.mobile}px) {
@@ -175,10 +201,10 @@ export const RepoLink = styled.a`
   };
 
   &:hover {
-    filter: brightness(120%);
+    filter: brightness(115%);
   };
 
   &:focus-visible {
-    filter: brightness(120%);
+    filter: brightness(115%);
   };
 `;
