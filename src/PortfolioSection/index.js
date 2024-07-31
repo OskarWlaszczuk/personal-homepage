2479c108b7c7668d4c52f629ad21cf7fc8d83438
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-31 18:04:49 +0200

    Fetching repositories after first page-render, with 1sec delay, setting theme styles, depending on isDarkTheme state

--- src/PortfolioSection/index.js
+++ src/PortfolioSection/index.js
@@ -1,17 +1,52 @@
-import { Container, HeaderContent, Header, SubHeader, StyledGitHubIcon, Repositories, Repo, RepoName, RepoDescription, RepoRepository, RepoHomepage, RepoLinksList, RepoLinkTitle, RepoLink } from "./styled";
-import { useGitHubRepoApi } from "../useGitHubRepoApi";
+import { useEffect } from "react";
+import {
+    Container,
+    HeaderContent,
+    Header,
+    SubHeader,
+    StyledGitHubIcon,
+    Repositories,
+    Repo,
+    RepoName,
+    RepoDescription,
+    RepoRepository,
+    RepoHomepage,
+    RepoLinksList,
+    RepoLinkTitle,
+    RepoLink
+} from "./styled";
+import {
+    fetchRepositories,
+    selectRepositories,
+    selectRepositoriesStatus
+} from "../portfolioSlice";
 import { Error } from "./Error";
 import { Loading } from "./Loading";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsDarkTheme } from "../themeSlice";
 
 export const PortfolioSection = () => {
-    const { repositories, status } = useGitHubRepoApi();
+    const repositories = useSelector(selectRepositories);
+    const status = useSelector(selectRepositoriesStatus);
+
+    const isDarkTheme = useSelector(selectIsDarkTheme);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const timeoutOfGettingResponseId = setTimeout(() => {
+            dispatch(fetchRepositories());
+        }, 1000);
+        return () => clearTimeout(timeoutOfGettingResponseId);
+    }, []);
 
     return (
         <Container>
             <HeaderContent>
-                <StyledGitHubIcon></StyledGitHubIcon>
-                <Header>Portfolio</Header>
-                <SubHeader>My recent projects</SubHeader>
+                <StyledGitHubIcon />
+                <Header $isDarkTheme={isDarkTheme} >
+                    Portfolio
+                </Header>
+                <SubHeader $isDarkTheme={isDarkTheme}>My recent projects</SubHeader>
             </HeaderContent>
             {
                 status === "loading" ?
@@ -25,19 +60,34 @@ export const PortfolioSection = () => {
                                     repositories.map((
                                         { name, description, html_url, homepage }
                                     ) => (
-                                        <Repo key={name}>
-                                            <RepoName>
+                                        <Repo
+                                            $isDarkTheme={isDarkTheme}
+                                            key={name}>
+                                            <RepoName $isDarkTheme={isDarkTheme}>
                                                 {name.charAt(0).toUpperCase() + name.slice(1)}
                                             </RepoName>
-                                            <RepoDescription>{description}</RepoDescription>
+                                            <RepoDescription $isDarkTheme={isDarkTheme}>
+                                                {description}
+                                            </RepoDescription>
                                             <RepoLinksList>
                                                 <RepoHomepage>
-                                                    <RepoLinkTitle>Demo:</RepoLinkTitle>
-                                                    <RepoLink key={homepage} target="_blank" href={homepage}> {homepage}</RepoLink>
+                                                    <RepoLinkTitle $isDarkTheme={isDarkTheme}>Demo:</RepoLinkTitle> <RepoLink
+                                                        key={homepage}
+                                                        target="_blank"
+                                                        href={homepage}
+                                                        $isDarkTheme={isDarkTheme}
+                                                    >
+                                                        {homepage}
+                                                    </RepoLink>
                                                 </RepoHomepage>
                                                 <RepoRepository>
-                                                    <RepoLinkTitle>Code:</RepoLinkTitle>
-                                                    <RepoLink key={html_url} target="_blank" href={html_url}> {html_url}</RepoLink>
+                                                    <RepoLinkTitle $isDarkTheme={isDarkTheme}>Code:</RepoLinkTitle> <RepoLink
+                                                        $isDarkTheme={isDarkTheme}
+                                                        key={html_url}
+                                                        target="_blank" href={html_url}
+                                                    >
+                                                        {html_url}
+                                                    </RepoLink>
                                                 </RepoRepository>
                                             </RepoLinksList>
                                         </Repo>
