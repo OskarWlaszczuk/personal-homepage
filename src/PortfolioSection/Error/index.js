414bb25c9e701e56481009fdd363c63bee7b1195
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-31 20:20:19 +0200

    Fixed Container import, formatted code

--- src/PortfolioSection/Error/index.js
+++ src/PortfolioSection/Error/index.js
@@ -1,25 +1,30 @@
-import { Title } from "./styled"
-import { Message } from "../../Message"
-import { Container } from "../../Container"
-import { LinkBox } from "../../LinkBox"
-import { useSelector } from "react-redux"
-import { selectIsDarkTheme } from "../../themeSlice"
-import { ReactComponent as ErrorIcon } from "../../svgs/Error.svg"
+import { Title } from "./styled";
+import { Message } from "../../Message";
+import { Container } from "../../Container";
+import { LinkBox } from "../../common/LinkBox";
+import { useSelector } from "react-redux";
+import { selectIsDarkTheme } from "../../themeSlice";
+import { ReactComponent as ErrorIcon } from "../../svgs/Error.svg";
 
 export const Error = () => {
-    const isDarkTheme = useSelector(selectIsDarkTheme);
+  const isDarkTheme = useSelector(selectIsDarkTheme);
 
-    return (
-        <Container>
-            <ErrorIcon />
-            <Title $isDarkTheme={isDarkTheme}>Ooops! Something went wrong...</Title>
-            <Message content={<>
-                Sorry, failed to load Github projects. <br />You can check them directly on Github.
-            </>} />
-            <LinkBox
-                href="https://github.com/OskarWlaszczuk/personal-homepage.git"
-                text="Go to Github"
-            />
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <ErrorIcon />
+      <Title $isDarkTheme={isDarkTheme}>Ooops! Something went wrong...</Title>
+      <Message
+        content={
+          <>
+            Sorry, failed to load Github projects. <br />
+            You can check them directly on Github.
+          </>
+        }
+      />
+      <LinkBox
+        href="https://github.com/OskarWlaszczuk/personal-homepage.git"
+        text="Go to Github"
+      />
+    </Container>
+  );
+};
