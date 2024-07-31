c0436f3c4fcc31ca6418223a9572ca6b7e18b3f2
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-07-31 20:21:46 +0200

    fixed Caption import, formatted code

--- src/ToggleThemeButton/index.js
+++ src/ToggleThemeButton/index.js
@@ -1,27 +1,26 @@
-import { Wrapper, StyledDarkThemeOnButton, StyledLightThemeOnButton } from "./styled";
-import { Caption } from "../Caption";
+import {
+  Wrapper,
+  StyledDarkThemeOnButton,
+  StyledLightThemeOnButton,
+} from "./styled";
+import { Caption } from "../common/Caption";
 import { toggleTheme } from "../themeSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { selectIsDarkTheme } from "../themeSlice";
 
 export const ToggleThemeButton = () => {
-    const isDarkTheme = useSelector(selectIsDarkTheme);
-    const dispatch = useDispatch();
-    const toggleThemeHandler = () => dispatch(toggleTheme());
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+  const dispatch = useDispatch();
+  const toggleThemeHandler = () => dispatch(toggleTheme());
 
-    return (
-        <Wrapper>
-            <Caption content={
-                isDarkTheme ?
-                    <>DARK MODE ON</> :
-                    <>DARK MODE OFF</>
-            } />
-            {isDarkTheme ?
-                <StyledDarkThemeOnButton
-                    onClick={toggleThemeHandler} /> :
-                <StyledLightThemeOnButton
-                    onClick={toggleThemeHandler} />
-            }
-        </Wrapper>
-    );
-}
+  return (
+    <Wrapper>
+      <Caption content={isDarkTheme ? <>DARK MODE ON</> : <>DARK MODE OFF</>} />
+      {isDarkTheme ? (
+        <StyledDarkThemeOnButton onClick={toggleThemeHandler} />
+      ) : (
+        <StyledLightThemeOnButton onClick={toggleThemeHandler} />
+      )}
+    </Wrapper>
+  );
+};
