e91bc5f2dec3fe21a8deb4185f1b99c8099966f2
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-08-25 08:37:55 +0200

    devided theme on three parts: lightTheme, darkTheme where each contains colors specific for theme, and breakPoints, which is available in the previous two

--- src/theme.js
+++ src/theme.js
@@ -1,33 +1,39 @@
-export const theme = {
-    lightTheme: {
-        colors: {
-            slateGray: "#6E7E91",
-            mineShaft: "#252525",
-            white: "#FFFFFF",
-            whiteLilac: "#eeeeee3d",
-            scienceBlue: "#0366D6",
-            lightScienceBlue: "#D1D5DA4D",
-            anakiwa: "#8CC2FF",
-            iron: "#D1D5DA",
-            cornflowerBlue: "#0366D633",
-            lightViolet: "#090a3345",
-        },
-    },
-    darkTheme: {
-        colors: {
-            dodgerBlue: "#2188FF",
-            mineShaft: "#252525",
-            lightMineShaft: "#363636B8",
-            white: "#FFFFFF",
-            lightIron: "#D1D5DA1A",
-            grayScienceBlue: "#0366D680",
-        },
-    },
-    breakPoints: {
-        laptop: 1100,
-        smallLaptop: 950,
-        tablet: 730,
-        mobileXl: 550,
-        mobile: 350,
-    },
+export const breakPoints = {
+  breakPoints: {
+    laptop: 1100,
+    smallLaptop: 950,
+    tablet: 730,
+    mobileXl: 550,
+    mobile: 350,
+  },
+};
+
+export const lightTheme = {
+  ...breakPoints,
+  colors: {
+    lightThemeMineShaft: "#252525",
+    slateGray: "#6E7E91",
+    lightThemeWhite: "#FFFFFF",
+    whiteLilac: "#eeeeee3d",
+    scienceBlue: "#0366D6",
+    lightScienceBlue: "#D1D5DA4D",
+    anakiwa: "#8CC2FF",
+    iron: "#D1D5DA",
+    cornflowerBlue: "#0366D633",
+    lightViolet: "#090a3345",
+  },
+};
+
+export const darkTheme = {
+  ...breakPoints,
+  colors: {
+    white: "#FFFFFF",
+    mineShaft: "#252525",
+    dodgerBlue: "#2188FF",
+    shipCove: "#6D93BE",
+    lightMineShaft: "#363636B8",
+    lightIron: "#D1D5DA1A",
+    grayScienceBlue: "#0366D680",
+    coldViolet: "#090A3308",
+  },
 };
